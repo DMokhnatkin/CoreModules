4dbc970dec0bfa675305445272f5971cf3775641
DMokhnatkin <dmitriy.mokhnatkin@gmail.com>
2017-03-09 21:19:21 +0300

    Folder moved

---
+++ VsGuiCore/Client/VsGuiCore.Client.Wpf/VsGuiCore.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace VsGuiCore.Client.Wpf
+{
+    public class VsGuiCore
+    {
+        public FrameworkElement MainMenu { get; set; }
+        public StatusBar StatusBar { get; set; }
+    }
+}
---
+++ VsGuiCore/Client/VsGuiCore.Client.Wpf/WpfClientEntry.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Practices.Unity;
+using ModularSystem.Common.Wpf;
+using ModularSystem.Common.Wpf.Context;
+using ModularSystem.Common.Wpf.UI;
+using TestGuiCore.Client.Wpf;
+
+namespace VsGuiCore.Client.Wpf
+{
+    public class WpfClientEntry : IWpfClientEntry
+    {
+        /// <inheritdoc />
+        public void OnInstalled()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public void OnRemoved()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public void OnStart()
+        {
+            MyMainUserControl t = new MyMainUserControl();
+            var p = ClientAppContext.CurrentContext.Container.Resolve<VsGuiCore>();
+            p.MainMenu = t.MainMenu;
+            p.StatusBar = t.StatusBar;
+            ClientAppContext.CurrentContext.Container.Resolve<MainUi>().MainContent = t;
+        }
+
+        /// <inheritdoc />
+        public void OnExit()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
